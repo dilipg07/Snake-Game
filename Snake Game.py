b8a138184b47fc4922c39477cc8bc56dbef9cd92
Dilip Gummadi <dilip.calgry@gmail.com>
2023-05-26 16:36:48 +0530

    imported time module to delay the snake movement. Turned off screen animation using 'tracer' method. Added logic to move the snake forward like a train.

--- Snake Game.py
+++ Snake Game.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("🐍 Snake Game 🐍")
+screen.tracer(0)
 
 start_position = [(0,0),(-20,0),(-40,0)]
 segment = []
@@ -17,6 +19,11 @@ for pos in start_position:
 
 game_on = True
 while game_on:
-    for seg in segment:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+    for seg in range(len(segment)-1,0,-1):
+        segment[seg].goto(segment[seg-1].xcor(), segment[seg-1].ycor())
+    segment[0].forward(20)
+    segment[0].left(90)
+        
 screen.exitonclick()
