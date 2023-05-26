e26a6c080a0da86cfa89ec5958ea21e6a14bc351
Dilip Gummadi <dilip.calgry@gmail.com>
2023-05-26 15:33:53 +0530

    added penup. Created a list to store squares. created a while loop to move the snake by 20px forward.

--- Snake Game.py
+++ Snake Game.py
@@ -6,10 +6,17 @@ screen.bgcolor("black")
 screen.title("🐍 Snake Game 🐍")
 
 start_position = [(0,0),(-20,0),(-40,0)]
-
+segment = []
 for pos in start_position:
     new_square = Turtle('square')
     new_square.color('white')
+    new_square.penup()
     new_square.goto(pos)
+    segment.append(new_square)
 
+
+game_on = True
+while game_on:
+    for seg in segment:
+        seg.forward(20)
 screen.exitonclick()
