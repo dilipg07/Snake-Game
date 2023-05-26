f4322221f058d851c03f2942dc4bfb62ac08c938
Dilip Gummadi <dilip.calgry@gmail.com>
2023-05-26 15:14:32 +0530

    created 3 white squares and arranged them in line.

--- Snake Game.py
+++ Snake Game.py
@@ -4,3 +4,12 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("🐍 Snake Game 🐍")
+
+start_position = [(0,0),(-20,0),(-40,0)]
+
+for pos in start_position:
+    new_square = Turtle('square')
+    new_square.color('white')
+    new_square.goto(pos)
+
+screen.exitonclick()
