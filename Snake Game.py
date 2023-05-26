cb0d8bf354b10b1be29e540370c790e0d74092eb
Dilip Gummadi <dilip.calgry@gmail.com>
2023-05-26 23:18:43 +0530

    Added key bindings to control the snake

--- Snake Game.py
+++ Snake Game.py
@@ -9,7 +9,11 @@ screen.title("🐍 Snake Game 🐍")
 screen.tracer(0)
 
 snake = Snake()
-
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 game_on = True
 while game_on:
     screen.update()
--- Snake.py
+++ Snake.py
@@ -18,3 +18,16 @@ class Snake:
         for seg in range(len(self.segment)-1,0,-1):
             self.segment[seg].goto(self.segment[seg-1].xcor(), self.segment[seg-1].ycor())
         self.segment[0].forward(move_distance)
+
+    def up(self):
+        if self.segment[0].heading() != 270:
+            self.segment[0].seth(90)
+    def down(self):
+        if self.segment[0].heading() != 90:
+            self.segment[0].seth(270)
+    def left(self):
+        if self.segment[0].heading() != 0:
+            self.segment[0].seth(180)
+    def right(self):
+        if self.segment[0].heading() != 180:    
+            self.segment[0].seth(0)
